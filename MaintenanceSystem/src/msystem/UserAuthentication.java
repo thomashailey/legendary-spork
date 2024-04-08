abf7ca19a563c10c0ded3f347fbfee634350532c
Mistaweelymon <162065192+Mistaweelymon@users.noreply.github.com>
2024-04-07 23:15:42 -0400

    userAuthentication class
    Working on the class so the I can clean up userauth main

--- MaintenanceSystem/src/msystem/UserAuthentication.java
+++ MaintenanceSystem/src/msystem/UserAuthentication.java
@@ -4,18 +4,57 @@
  */
 package msystem;
 
+import java.sql.*;
 /**
  *
  * @author thoma
  */
 public class UserAuthentication {
+    DBConnect db = new DBConnect();
+    Connection con = null;
+    PreparedStatement stmt;
+    ResultSet result;
+    String sql = null;
     
-    private void AuthenticateUser() {
-        
+    
+    
+    public boolean authenticateUser(String userCode, String password) {
+        try {
+            if (checkPrimaryKey(userCode, password)) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private boolean checkPrimaryKey(String userCode, String password) {
+        try {
+            // Get database connection
+            con = db.OpenConnection();
+
+            // Prepare SQL statement
+            String sql = "SELECT * FROM user_authoization WHERE UserID = ? AND Password = ?";
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, userCode);
+            statement.setString(2, password);
+
+            // Execute query
+            ResultSet resultSet = statement.executeQuery();
+            boolean result = resultSet.next();
+
+            // Close database connection
+            con.close();
+
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
     
-    private void CheckAccess() {
-        
-    }
-    
+
+
 }
