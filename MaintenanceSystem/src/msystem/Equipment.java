8df844798d03b9e8ad9a41785c15601c5d2da4f9
GitHubVaughn <78117361+GitHubVaughn@users.noreply.github.com>
2024-04-09 18:07:19 -0500

    Updated Equipment Class Code

--- MaintenanceSystem/src/msystem/Equipment.java
+++ MaintenanceSystem/src/msystem/Equipment.java
@@ -1,22 +1,15 @@
 package msystem;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 
 /**
  *
  * @author thoma / vaughnr
  */
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Equipment {
     
@@ -26,19 +19,107 @@ public class Equipment {
     PreparedStatement stmt;
     ResultSet result;
     
-    public void CheckIn() {
+    public ArrayList<String> CheckIn() throws SQLException, ClassNotFoundException {
         // Access database to return equipment
+             con = db.OpenConnection();
+        ArrayList<String> elements = new ArrayList<>();
+        String itemToAdd = null;
+        try {
+                 String sql = "SELECT * FROM equipment";
+            stmt = con.prepareStatement(sql);
+            
+            result = stmt.executeQuery();
+            
+            if (result != null) {
+                System.out.println("Successfully Accessed Equipment DataBase");
+            }
+            while (result.next()) {
+                itemToAdd = String.format("%s -- %s", result.getString("EquipmentName"), result.getString("Description"));
+                if(!elements.contains(itemToAdd)){
+                    elements.add(itemToAdd);
+                }
+            }
+        }
+        catch(Exception e) {
+            System.out.println(e);
+            System.out.println("Equipment.Checkin");
+        }
+        return elements;   
      }
     
-    public void CheckOut() {
-        // Access database to assign equipment and check out
+    public ArrayList<String> CheckOut() throws SQLException, ClassNotFoundException {
+        // Access database to assign equipment and check out        
+        con = db.OpenConnection();
+        ArrayList<String> elements = new ArrayList<>();
+        String itemToAdd = null;
+        try {
+            String sql = "SELECT * FROM equipment";
+            stmt = con.prepareStatement(sql);
+            
+            result = stmt.executeQuery();
+            
+            if (result != null) {
+                System.out.println("Successfully Accessed Equipment DataBase");
+            }
+            while (result.next()) {
+                itemToAdd = String.format("%s -- %s", result.getString("EquipmentName"), result.getString("Description"));
+                if(!elements.contains(itemToAdd)){
+                    elements.add(itemToAdd);
+                }
+            }
+        }
+        catch(Exception e) {
+            System.out.println(e);
+            System.out.println("Equipment.Checkout");
+        }
+        return elements;
+        
     }
     
-    public void ViewEquipment(){
+    public void AdjustEquipment(){
         // 
     }
+
     public void ReportLoss() {
         // Report loss of equipment
+    }
+    
+    /**
+     *
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    public ArrayList ViewEquipment() throws SQLException, ClassNotFoundException {
+        /*  Set connection to DBConnect OpenConnection() method,
+            Create ArrayList to store DB elements
+        */
+        //need to find a way to ensure that dupe items are not added to the list, then pull both when the request is bigger than the primary
+        con = db.OpenConnection();
+        ArrayList<String> elements = new ArrayList<>();
+        String itemToAdd = null;
+        try {
+            String sql = "SELECT * FROM equipment";
+            stmt = con.prepareStatement(sql);
+            
+            result = stmt.executeQuery();
+            
+            if (result != null) {
+                System.out.println("Successfully Accessed Equipment DataBase");
+            }
+            while (result.next()) {
+                itemToAdd = String.format("%s -- %s", result.getString("EquipmentName"), result.getString("Description"));
+                if(!elements.contains(itemToAdd)){
+                    elements.add(itemToAdd);
+                }
+            }
+        }
+        catch(Exception e) {
+            System.out.println(e);
+            System.out.println("Equipment.ViewEquipment");
+        }
+        return elements;
+        
     }
     
     public void CheckStatus() {
@@ -102,6 +183,7 @@ public class Equipment {
         
     }
     
+
  public ArrayList ViewEquipment(String item) throws SQLException, ClassNotFoundException {
         // Search inventory for specific item
         /*  Set connection to DBConnect OpenConnection() method,
